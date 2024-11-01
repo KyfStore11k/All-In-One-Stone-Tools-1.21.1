c2f63ac6cfb7b4bec3d0a6284a1e5f8d530b0568
KyfStore <dfung815@gmail.com>
2024-11-01 17:24:46 -0400

    Initial commit

---
+++ src/main/java/com/kyfstore/fabricmc/AllInOneStoneToolsClient.java
@@ -0,0 +1,10 @@
+package com.kyfstore.fabricmc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AllInOneStoneToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/kyfstore/fabricmc/AllInOneStoneToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kyfstore.fabricmc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AllInOneStoneToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
